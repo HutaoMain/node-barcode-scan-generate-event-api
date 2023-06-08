e38c9f2fa99fe86d8ce8050eceabdad63eed5c0c
HutaoMain <alimohamedalcantara@gmail.com>
2023-06-09 02:43:12 +0800

    edit cors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,13 @@ dotEnv.config();
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://127.0.0.1:5173", process.env.CLIENT_URL],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 //mongoose connection here
 mongoose.set("strictQuery", false);
