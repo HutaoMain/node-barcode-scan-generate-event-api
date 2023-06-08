9f078df1dcfcc54c6434e913bd0e5e9247b2cf2e
HutaoMain <alimohamedalcantara@gmail.com>
2023-06-09 02:48:41 +0800

    edit cors

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173", process.env.CLIENT_URL],
+    origin: "*",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
