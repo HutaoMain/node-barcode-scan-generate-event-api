953d2018c097cee40b647a8fd7d4b91b132a063d
HutaoMain <alimohamedalcantara@gmail.com>
2023-06-09 02:55:04 +0800

    edit cors

--- src/index.js
+++ src/index.js
@@ -12,13 +12,7 @@ dotEnv.config();
 const app = express();
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 //mongoose connection here
 mongoose.set("strictQuery", false);
