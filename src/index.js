fe84c97afeadbd148d8e986e612e1f2ada377b4c
HutaoMain <alimohamedalcantara@gmail.com>
2023-06-09 07:13:19 +0800

    add idnumber in usermodel

--- src/index.js
+++ src/index.js
@@ -12,12 +12,7 @@ dotEnv.config();
 const app = express();
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    methods: "GET,POST,PUT,DELETE",
-  })
-);
+app.use(cors("*"));
 
 //mongoose connection here
 mongoose.set("strictQuery", false);
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -14,7 +14,7 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    IdNumber: {
+    idNumber: {
       type: String,
       required: true,
     },
